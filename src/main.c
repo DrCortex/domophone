9d3622f5f5cb4111d097bbd021d6737885f4695e
DrCortex <patryk.kups1@gmail.com>
2024-04-25 21:39:22 +0200

    initial commit, clean rtos added

--- src/main.c
+++ src/main.c
@@ -1,28 +1,28 @@
 #include "FreeRTOS.h"
+#include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
 #include "task.h"
 #include <stdio.h>
-#include "pico/stdlib.h"
 
-
-void led_task()
-{   
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        vTaskDelay(100);
-        gpio_put(LED_PIN, 0);
-        vTaskDelay(100);
-    }
+void led_task() {
+  if (cyw43_arch_init()) {
+    printf("Wi-Fi init failed");
+    return -1;
+  }
+  while (true) {
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    sleep_ms(250);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+    sleep_ms(250);
+  }
 }
 
-int main()
-{
-    stdio_init_all();
+int main() {
+  stdio_init_all();
 
-    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
-    vTaskStartScheduler();
+  xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+  vTaskStartScheduler();
 
-    while(1){};
+  while (1) {
+  };
 }
